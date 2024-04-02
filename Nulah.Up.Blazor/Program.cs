5de441665478767ec202a892bdfb552a31b66423
Scott <scott.schultz@jotunga.com>
2024-04-02 10:28:25 +1000

    Add environment variable prefix

--- Nulah.Up.Blazor/Program.cs
+++ Nulah.Up.Blazor/Program.cs
@@ -14,6 +14,8 @@ public class Program
 	public static void Main(string[] args)
 	{
 		var builder = WebApplication.CreateBuilder(args);
+		
+		builder.Configuration.AddEnvironmentVariables(prefix: "NulahUpBank_");
 
 		// Add services to the container.
 		builder.Services.AddRazorComponents()
